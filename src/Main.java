a4d1bcdc32cefad470a522d9fc4eae5b40e3c8e2
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-10-04 21:20:09 -0300

    alterações para notificações para compras não pagas

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,37 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        // Lista de compras
+
         List<Compra> compras = new ArrayList<>();
         compras.add(new Compra("Notebook", 2500.00, true));
         compras.add(new Compra("Teclado", 150.00, false));
         compras.add(new Compra("Mouse", 100.00, true));
 
-        // Interface funcional para filtrar compras pagas
+
         Predicate<Compra> filtroCompraPaga = compra -> compra.getIsPago();
 
-        // Filtrando as compras pagas
+
         compras.stream()
                 .filter(filtroCompraPaga)
                 .forEach(System.out::println);
+
+
+        Notificacao enviarEmail = compra -> System.out.println("Enviando e-mail para a compra: " + compra.getProduto());
+        Notificacao enviarSMS = compra -> System.out.println("Enviando SMS para a compra: " + compra.getProduto());
+
+        System.out.println("\nNotificações para compras não pagas:");
+
+        compras.stream()
+                .filter(compra -> !compra.getIsPago())  // Compras não pagas
+                .forEach(compra -> {
+                    enviarEmail.enviar(compra);  // Simulando envio por e-mail
+                    enviarSMS.enviar(compra);    // Simulando envio por SMS
+                });
     }
+}
+
+
+@FunctionalInterface
+interface Notificacao {
+    void enviar(Compra compra);
 }
